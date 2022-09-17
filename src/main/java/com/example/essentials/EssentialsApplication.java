433b1f68a079ea47fe9d307733e934b2eccc154c
jayground8 <jayground8@gmail.com>
2022-09-17 15:41:47 +0900

    simple example

---
+++ src/main/java/com/example/essentials/EssentialsApplication.java
@@ -0,0 +1,24 @@
+package com.example.essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class EssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EssentialsApplication.class, args);
+    }
+
+}
+
+@RestController
+class TestController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Hello world!";
+    }
+}
